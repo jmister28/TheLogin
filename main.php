f28210a6b30004ca832567b4522a8cb0d3b235b4
jmister28 <jmister28@users.noreply.github.com>
2015-04-04 18:35:20 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,3 +1,10 @@
 <?php
 
 ?>
+<html>
+  <form action= "login.php" method= "POST">
+    <input type= 'text' name= 'username'> <br>
+    <iput type= 'password' name= 'password'> <br>
+    <input type= 'submit' name= 'button' value= 'login'>
+  </form>
+</html>
